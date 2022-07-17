9990954bc5b2dd3634930c2a6d9f16a18495d192
yom <yominzoku@gmail.com>
2022-07-18 01:57:43 +0900

    Initial Commit

---
+++ ProgressLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressLog
+//
+//  Created by Yo Tahara on 2022/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
